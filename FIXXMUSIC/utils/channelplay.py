1bddcf3a2d40a408b64eeff7e4849b83420c31d5
SKD-VASHU <165390858+Vashu2456@users.noreply.github.com>
2024-05-16 22:01:43 +0530

    Update channelplay.py

--- FIXXMUSIC/utils/channelplay.py
+++ FIXXMUSIC/utils/channelplay.py
@@ -1,5 +1,5 @@
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import get_cmode
+from FIXXMUSIC import app
+from FIXXMUSIC.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
