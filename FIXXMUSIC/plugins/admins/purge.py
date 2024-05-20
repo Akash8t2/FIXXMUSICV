101361cbc180ebed095ecff07cc5ec29f1e72a05
SKD-VASHU <165390858+Vashu2456@users.noreply.github.com>
2024-05-20 23:04:01 +0530

    Update purge.py

--- FIXXMUSIC/plugins/admins/purge.py
+++ FIXXMUSIC/plugins/admins/purge.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import MessageDeleteForbidden, RPCError
 from pyrogram.types import Message
-from FIXXMUSIC.utils.daxx_ban import admin_filter
+from FIXXMUSIC.utils.fixx_ban import admin_filter
 from FIXXMUSIC import app
 
 
