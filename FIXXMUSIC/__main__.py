a52eb94ceff995cc3489b7df6449623c1d0d8c6d
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-26 08:19:00 +0530

    F9

--- FIXXMUSIC/__main__.py
+++ FIXXMUSIC/__main__.py
@@ -1,6 +1,6 @@
 import asyncio
 import importlib
-from sys import argv
+
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
@@ -12,8 +12,6 @@ from FIXXMUSIC.plugins import ALL_MODULES
 from FIXXMUSIC.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
-from VIPMUSIC.plugins.tools.clone import restart_bots
-
 
 async def init():
     if (
@@ -23,10 +21,8 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error(
-            "𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 V2 𝐒𝐞𝐬𝐬𝐢𝐨𝐧🤬"
-        )
-
+        LOGGER(__name__).error("𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐒𝐞𝐬𝐬𝐢𝐨𝐧")
+        exit()
     await sudo()
     try:
         users = await get_gbanned()
@@ -34,31 +30,33 @@ async def init():
             BANNED_USERS.add(user_id)
         users = await get_banned_users()
         for user_id in users:
-            if user_id not in BANNED_USERS:
-                BANNED_USERS.add(user_id)
+            BANNED_USERS.add(user_id)
     except:
         pass
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("FIXXMUSIC.plugins" + all_module)
     LOGGER("FIXXMUSIC.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
-
     await userbot.start()
-
     await FIXX.start()
+    try:
+        await FIXX.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("FIXXMUSIC").error(
+            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\FIXX 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
+        )
+        exit()
+    except:
+        pass
     await FIXX.decorators()
-    await restart_bots()
-    LOGGER("FIXXMUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝙑𝘼𝙎𝙃𝙐♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    LOGGER("FIXXMUSIC").info(
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 FIXX☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+    )
     await idle()
-
     await app.stop()
     await userbot.stop()
-
-    LOGGER("FIXXMUSIC").info(
-        "                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝙑𝘼𝙎𝙃𝙐♨️\n╚═════ஜ۩۞۩ஜ════╝"
-    )
+    LOGGER("FIXXMUSIC").info("𝗦𝗧𝗢𝗣 FIXX 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
 
 
 if __name__ == "__main__":
-
     asyncio.get_event_loop().run_until_complete(init())
